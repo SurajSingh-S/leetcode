435370ece48d5dad5059c901810d388d00a4bec3
SurajSingh-S <146805274+SurajSingh-S@users.noreply.github.com>
2025-05-11 12:49:14 +0530

    Time: 36 ms (51.18%) | Memory: 44.6 MB (42.92%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.java
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.java
@@ -2,10 +2,12 @@ class Solution {
     public int minimumMountainRemovals(int[] nums) {
         int [] left= new int [nums.length];
         int [] right= new int [nums.length];
+        Arrays.fill(left,1);
+        Arrays.fill(right,1);
         int n = nums.length;
 
         for(int i=0;i<n;i++){
-            left[i]=1;
+           // left[i]=1;
             for(int j=0;j<i;j++){
                 if(nums[i]>nums[j]){
                     left[i]=Math.max(left[i],left[j]+1);
@@ -14,7 +16,7 @@ class Solution {
         }
 
         for(int i=n-1;i>=0;i--){
-            right[i]=1;
+          //  right[i]=1;
             for(int j=n-1;j>i;j--){
                 if(nums[i]>nums[j]){
                     right[i]=Math.max(right[i],right[j]+1);
