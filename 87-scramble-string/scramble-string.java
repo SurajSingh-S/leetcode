986ed11afbb7831e0b25e9219a2e3c2c84cdf615
SurajSingh-S <146805274+SurajSingh-S@users.noreply.github.com>
2025-05-25 21:28:17 +0530

    Time: 68 ms (27.15%) | Memory: 45.5 MB (41.24%) - LeetSync

--- 87-scramble-string/scramble-string.java
+++ 87-scramble-string/scramble-string.java
@@ -1,55 +1,132 @@
+// class Solution {
+//         private Map<String, Boolean> memo = new HashMap<>();
+
+//     public boolean isScramble(String s1, String s2) {
+//         int n= s1.length();
+//         if(s1.equals(s2)) return true;
+//         if(s1.length() != s2.length()) return false;
+
+//         String key= s1 +'#'+s2;
+
+//         if(memo.containsKey(key)) {
+//             return memo.get(key);
+//         }
+
+//         if(!havesamechar(s1,s2)){
+//             memo.put(key,false);
+//             return false;
+//         }
+
+//         for(int i=1;i<n;i++){
+//             //check without swap
+//                 if(isScramble(s1.substring(0,i), s2.substring(0,i)) && isScramble(s1.substring(i),s2.substring(i)) ){
+//                     memo.put(key,true);
+//                     return true;
+//                 }
+
+//             //check with swap
+
+//             if(isScramble(s1.substring(0,i),s2.substring(n-i)) && isScramble(s1.substring(i),s2.substring(0,n-i))){
+//                    memo.put(key,true);
+//                    return true;
+//             }
+//         }
+//         memo.put(key,false);
+//         return false;
+
+
+//     }
+
+//     public boolean havesamechar (String s1, String s2){
+//         int [] arr= new int [26];
+//         for(char i:s1.toCharArray()){
+//             arr[i-'a']++;
+//         }
+//         for(char i:s1.toCharArray()){
+//             arr[i-'a']--;
+//         }
+
+//         for(int i:arr){
+//             if(i != 0) return false;
+//         }
+//         return true;
+//     }
+// }
+
+
 class Solution {
-        private Map<String, Boolean> memo = new HashMap<>();
+    // for storing already solved problems
+    Map<String, Boolean> mp = new HashMap<>();
 
     public boolean isScramble(String s1, String s2) {
-       // Map<String,Integer> memo= new HashMap<>();
-        int n= s1.length();
-        if(s1.equals(s2)) return true;
-        if(s1.length() != s2.length()) return false;
+        int n = s1.length();
 
-        String key= s1 +'#'+s2;
-
-        if(memo.containsKey(key)) {
-            return memo.get(key);
-        }
-
-        if(!havesamechar(s1,s2)){
-            memo.put(key,false);
+        // if both strings are not equal in size
+        if (s2.length() != n)
             return false;
-        }
 
-        for(int i=1;i<n;i++){
-            //check without swap
-                if(isScramble(s1.substring(0,i), s2.substring(0,i)) && isScramble(s1.substring(i),s2.substring(i)) ){
-                    memo.put(key,true);
-                    return true;
-                }
+        // if both strings are equal
+        if (s1.equals(s2))
+            return true;
 
-            //check with swap
+        // if code is reached to this condition then following this are sure:
+        // 1. size of both string is equal
+        // 2. string are not equal
+        // so size is equal (where size==1) and they are not equal then obviously false
+        // example 'a' and 'b' size is equal ,string are not equal
+        if (n == 1)
+            return false;
 
-            if(isScramble(s1.substring(0,i),s2.substring(n-i)) && isScramble(s1.substring(i),s2.substring(0,n-i))){
-                   memo.put(key,true);
-                   return true;
+        String key = s1 + " " + s2;
+
+        // check if this problem has already been solved
+        if (mp.containsKey(key))
+            return mp.get(key);
+
+        // for every iteration it can two condition
+        // 1.we should proceed without swapping
+        // 2.we should swap before looking next
+        for (int i = 1; i < n; i++) {
+            // ex of without swap: gr|eat and rg|eat
+            boolean withoutswap = (
+                    // left part of first and second string
+                    isScramble(s1.substring(0, i), s2.substring(0, i))
+
+                            &&
+
+                            // right part of first and second string;
+                            isScramble(s1.substring(i), s2.substring(i))
+            );
+
+            // if without swap give us right answer then we do not need
+            // to call the recursion withswap
+            if (withoutswap) {
+                mp.put(key, true);
+                return true;
             }
+
+            // ex of withswap: gr|eat rge|at
+            // here we compare "gr" with "at" and "eat" with "rge"
+            boolean withswap = (
+                    // left part of first and right part of second
+                    isScramble(s1.substring(0, i), s2.substring(n - i))
+
+                            &&
+
+                            // right part of first and left part of second
+                            isScramble(s1.substring(i), s2.substring(0, n - i))
+            );
+
+            // if withswap give us right answer then we return true
+            // otherwise the for loop do it work
+            if (withswap) {
+                mp.put(key, true);
+                return true;
+            }
+            // we are not returning false in else case
+            // because we want to check further cases with the for loop
         }
-        memo.put(key,false);
+        mp.put(key, false);
         return false;
-
-
-    }
-
-    public boolean havesamechar (String s1, String s2){
-        int [] arr= new int [26];
-        for(char i:s1.toCharArray()){
-            arr[i-'a']++;
-        }
-        for(char i:s1.toCharArray()){
-            arr[i-'a']--;
-        }
-
-        for(int i:arr){
-            if(i != 0) return false;
-        }
-        return true;
     }
 }
