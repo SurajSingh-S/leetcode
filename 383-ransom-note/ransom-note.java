7338e039e89ff56833fe6179b1c80c3f6101b7fa
SurajSingh-S <146805274+SurajSingh-S@users.noreply.github.com>
2025-07-31 10:46:06 +0530

    Time: 53 ms (5.67%) | Memory: 45.5 MB (16.64%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int count=0;
+        char[] ransomNotearr=ransomNote.toCharArray();
+        char[] magazinearr=magazine.toCharArray();
+        Arrays.sort(ransomNotearr);
+        Arrays.sort(magazinearr);
+        for(int i=0;i<ransomNotearr.length;i++){
+            for(int j=0;j<magazinearr.length;j++){
+                if(ransomNotearr[i]==magazinearr[j]) {
+                count++;
+                magazinearr[j]='*';
+                break;
+                }
+            }
+        }
+         return(count==ransomNote.length()) ? true:false;
+
+        
+    }
+}
