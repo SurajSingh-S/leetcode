27eb3c728f91a337088ae07d2649a2357f2f4754
SurajSingh-S <146805274+SurajSingh-S@users.noreply.github.com>
2025-08-04 12:14:58 +0530

    Time: 11 ms (39.60%) | Memory: 44.8 MB (74.24%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,21 +1,32 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int count=0;
+        // int count=0;
         char[] ransomNotearr=ransomNote.toCharArray();
         char[] magazinearr=magazine.toCharArray();
-        // Arrays.sort(ransomNotearr);
-        // Arrays.sort(magazinearr);
-        for(int i=0;i<ransomNotearr.length;i++){
-            for(int j=0;j<magazinearr.length;j++){
-                if(ransomNotearr[i]==magazinearr[j]) {
-                count++;
-                magazinearr[j]='*';
-                break;
-                }
-            }
-        }
-         return(count==ransomNote.length()) ? true:false;
+       
+        // for(int i=0;i<ransomNotearr.length;i++){
+        //     for(int j=0;j<magazinearr.length;j++){
+        //         if(ransomNotearr[i]==magazinearr[j]) {
+        //         count++;
+        //         magazinearr[j]='*';
+        //         break;
+        //         }
+        //     }
+        // }
+        // return(count==ransomNote.length()) ? true:false;
 
-        
+        Map<Character,Integer>map= new HashMap<>();
+
+        for(char c:magazinearr){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        for(char c : ransomNotearr){
+            if( ! map.containsKey(c) || map.get(c)==0) return false;
+
+            else 
+            map.put(c,map.get(c)-1);
+        }   
+            return true;
     }
 }
