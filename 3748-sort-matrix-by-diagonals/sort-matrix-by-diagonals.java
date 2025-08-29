c9fd6a3756fa50c28506f40bb385c6edc1b965c0
SurajSingh-S <146805274+SurajSingh-S@users.noreply.github.com>
2025-08-29 14:33:29 +0530

    Time: 5 ms (70.35%) | Memory: 45.3 MB (96.46%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
@@ -1,41 +1,41 @@
-import java.util.*;
-
 class Solution {
     public int[][] sortMatrix(int[][] grid) {
-        int n = grid.length;
+        int n= grid.length;
 
-        // Top-right diagonals: start on first row, EXCLUDING main diagonal
-        for (int j = 1; j < n; ++j) {
-            sortDiagonal(grid, 0, j, true);   // ascending
+        for(int j=1;j<n;j++) {
+            sortDiagonal(grid,0,j,true);
         }
 
-        // Bottom-left diagonals: start on first column, INCLUDING main diagonal
-        for (int i = 0; i < n; ++i) {
-            sortDiagonal(grid, i, 0, false);  // descending
+        for(int i=0;i<n;i++){
+            sortDiagonal(grid,i,0,false);
         }
-
         return grid;
     }
 
-    private void sortDiagonal(int[][] g, int r, int c, boolean asc) {
-        int n = g.length;
-        List<Integer> vals = new ArrayList<>();
-        int i = r, j = c;
+    public void sortDiagonal(int [][]grid,int r,int c,boolean asc){
+        int i=r;
+        int j=c;
+        int n=grid.length;
 
-        // collect
-        while (i < n && j < n) {
-            vals.add(g[i++][j++]);
+        List<Integer> res= new ArrayList<>();
+
+        while(i<n && j<n){
+            res.add(grid[i++][j++]);
         }
 
-        // sort
-        if (asc) Collections.sort(vals);
-        else vals.sort(Collections.reverseOrder());
+        if(asc) Collections.sort(res);
 
-        // write back
-        i = r; j = c;
-        int k = 0;
-        while (i < n && j < n) {
-            g[i++][j++] = vals.get(k++);
+        else res.sort(Collections.reverseOrder());
+
+        i=r;
+        j=c;
+        int k=0;
+
+        while(i<n && j<n){
+            grid[i++][j++]=res.get(k++);
         }
+
     }
+
+
 }
