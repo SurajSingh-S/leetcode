5cdf9e1cebef9669a6a37ba2eb0a9f3add4547d9
SurajSingh-S <146805274+SurajSingh-S@users.noreply.github.com>
2025-09-07 19:05:45 +0530

    Time: 25 ms (18.09%) | Memory: 49.4 MB (38.66%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cs
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cs
@@ -1,15 +1,17 @@
 public class Solution {
     public int FirstUniqChar(string s) {
-        int [] arr= new int [26];
+        Dictionary<char,int> map= new Dictionary<char,int>();
 
         foreach(char c in s){
-            arr[c-'a']++;
+            if(map.ContainsKey(c)){
+                map[c]++;
+            }
+            else 
+                map[c]=1;
         }
 
         for(int i=0;i<s.Length;i++){
-            if(arr[s[i]-'a']==1){
-                return i;
-            }
+            if(map[s[i]]==1) return i;
         }
         return -1;
     }
